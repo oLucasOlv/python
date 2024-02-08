f14bb57ee829a4e0cc20f2d8afdde569ef842464
Lucas Silva Oliveira <127355660+oLucasOlv@users.noreply.github.com>
2024-02-08 16:09:45 -0300

    criada aula08 a b c

---
+++ diretorio-python/aulas/testes/aula08a.py
@@ -0,0 +1,7 @@
+# importar uma biblioteca por completo - import math
+
+from math import sqrt,floor # importar uma ou mais função especifica da biblioteca
+num = int(input('numero: '))
+raiz = sqrt(num)
+
+print(f'raiz quadrada: {floor.raiz:.2f}')
---
+++ diretorio-python/aulas/testes/aula08c.py
@@ -0,0 +1,3 @@
+import emoji
+
+print(emoji.emojize('Hello, World :smiling_face_with_open_hands:'))
